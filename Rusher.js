57969ed0fa84c04f5e01c1cc75708af26f1561fa
Nikolay Panayotov <nickpanaiotov@gmail.com>
2018-07-11 08:11:35 +0300

    Added push to remote collector on every block.

--- Rusher.js
+++ Rusher.js
@@ -1,40 +1,69 @@
 const ethers = require('ethers');
-const log = require('log-to-file');
+const {unsign} = require('@warren-bank/ethereumjs-tx-sign');
+var request = require('request');
 
-let lastBlock = 5932382;
-let addresses = [];
+let lastBlock = 5932378;
+
+class Block {
+    constructor(id, count, addresses) {
+        this.id = id;
+        this.count = count;
+        this.wallets = addresses;
+    }
+}
+
+class Address {
+    constructor(publicKey, address, amount) {
+        this.publicKey = publicKey;
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+function send(block) {
+    let requestBody = JSON.stringify(block);
+    console.log(requestBody);
+    request.post({
+        url: 'https://a3f4a9af.ngrok.io/blocks',
+        body: block,
+        json: true
+    });
+}
 
 async function rusher() {
     console.log("Rush");
 
     let providers = ethers.providers;
     let provider = providers.getDefaultProvider('mainnet');
-    let isWritten = false;
 
     for (i = lastBlock; i > 0; i--) {
-        log("Block Number: " + i);
+        var addresses = [];
+        var keys = [];
         console.log("Block Number: " + i);
         let block = await provider.getBlock(i);
         let transactions = block.transactions;
 
-        log("Transactions: " + transactions.length);
         console.log("Transactions: " + transactions.length);
 
         for (let j = 0; j < transactions.length; j++) {
             let transaction = await provider.getTransaction(transactions[j]);
-            isWritten = false;
-            for (let k = 0; k < addresses.length; k++) {
-                if (addresses[k] === transaction.to) {
-                    isWritten = true;
-                }
+            let rawMessage = transaction.raw;
+            let {publicKey, address} = unsign(rawMessage);
+
+            let balance = await provider.getBalance(address);
+            if (balance.toString() === "0") {
+                continue;
             }
-            if (!isWritten) {
-                addresses.push(transaction.to);
-                log(transaction.to);
-                console.log(transaction.to)
+
+            let entry = new Address(publicKey, address, balance.toString());
+
+            if (!keys.includes(publicKey)) {
+                keys.push(publicKey);
+                addresses.push(entry);
             }
         }
 
+        send(new Block(i, addresses.length, addresses));
     }
 }
 
